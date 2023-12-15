f03149f3daf33baf7d7113b0a7e388d97062fa45
Siddharth Kumar <149534917+thesiddyyy@users.noreply.github.com>
2023-12-16 02:27:15 +0530

    Add files via upload

---
+++ Python Strings Method.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Strings Method
+
+# In[12]:
+
+
+a = "my name is SidDharth Roy"
+a.capitalize()
+
+
+# In[13]:
+
+
+a = "My name is Siddharth Roy"
+a.casefold()
+
+
+# In[28]:
+
+
+a = "my name is Siddharth Roy"
+a.center(5)
+
+
+# In[26]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.count("2")
+
+
+# In[31]:
+
+
+a = "my name is Siddharth Roy"
+a.encode()
+
+
+# In[37]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.endswith("3")
+
+
+# In[38]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.expandtabs()
+
+
+# In[42]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.find("d")
+
+
+# In[43]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.format()
+
+
+# In[46]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.format_map("am")
+
+
+# In[50]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.index("am")
+
+
+# In[51]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isalnum()
+
+
+# In[52]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isalpha()
+
+
+# In[53]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isascii()
+
+
+# In[56]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isdecimal()
+
+
+# In[57]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isdigit()
+
+
+# In[58]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isidentifier()
+
+
+# In[59]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.islower()
+
+
+# In[60]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isnumeric()
+
+
+# In[61]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isprintable()
+
+
+# In[62]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isspace()
+
+
+# In[63]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.istitle()
+
+
+# In[64]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.isupper()
+
+
+# In[75]:
+
+
+a = "My name is Siddharth Roy , I am 22 years old"
+a.lower()
+
+
+# In[79]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.replace("my","Hello my")
+
+
+# In[81]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.rfind("name")
+
+
+# In[91]:
+
+
+a = "my name is Siddharth Roy , I am 22 years old"
+a.upper()
+
+
+# In[ ]:
+
+
+
+
